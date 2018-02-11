14d5e37c20203cb377b01c4487066c89bcb383c7
Lily Hoskin <Lily418@users.noreply.github.com>
2018-02-11 23:32:11 +0000

    Add /cat PUT endpoint

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const expressMongoDb = require("express-mongo-db")
 const express = require("express")
-const config = require('config')
+const config = require("config")
+const multer = require("multer")
 const app = express()
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 let connect = null
 
 app.use(expressMongoDb(config.get("dbConfig.mongoConnectionString"), config.get("dbConfig.dbName")));
 
-app.route("/cat").get((req, res) => { 
+app.get("/cat", (req, res) => { 
   const location = JSON.parse(req.query.location)
   return req.db.collection("cats").find({
     location: {
@@ -22,6 +25,25 @@ app.route("/cat").get((req, res) => {
     return res.status(200).json(cats);
   })
 })
-                  .put((req, res) => res.status(501).send("Not Implemented"))
+
+app.put("/cat", upload.single('cat'), (req, res) => {
+  const location = JSON.parse(req.query.location)
+  const catPhotoBuffer = req.file.buffer
+  req.db.collection("cats").insert({
+    "location": {
+      "type": "Point",
+      "coordinates": location
+    },
+    "photo": catPhotoBuffer
+  }).then((result) => {
+    if(result.result.ok) {
+      return res.status(200).json({"created": result.insertedIds[0]})
+    } else {
+      return res.status(500).json({ "error" : "Could not create cat"})
+    }
+  }).catch((err) => {
+    return res.status(500).json({ "error" : "Could not create cat"})
+  })
+})
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"))
